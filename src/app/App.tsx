b23bead66f388e30c5af3f001b60cf281dc41d28
Nikita1025 <nekit1025l@gmail.com>
2023-09-09 19:31:42 +0300

    refactor

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -12,6 +12,7 @@ import { useAppSelector } from '../store/store';
 
 const App = (): ReactElement => {
   const { status } = useAppSelector(state => state.post);
+  //s
   return (
     <div className="App">
       <Header />
